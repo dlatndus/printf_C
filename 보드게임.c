72bb5bdc42050ed7beaeed4eabf059d3f4063911
dlatndus <81746616+dlatndus@users.noreply.github.com>
2021-05-27 14:08:30 +0900

    최대카에 도달할 때만 게임을 끝내기
    최대칸을 초과한 경우 직정의 주사위 던진것을 무효처리함

--- 보드게임.c
+++ 보드게임.c
@@ -1,21 +1,24 @@
+/*
+*	보드게임 만들기 21.05.13
+*	1. 전체 칸의 갯수 61개
+*	2. 플레이어가 가지고 있어야 할 것
+*		- 위치정보(말) : 0 부터 시작
+*		- 돈 : 5,000,000 부터 시작
+*		- 아이템 인벤토리 (나중에 가능하면)
+*	3. 파산의 기준
+*		- 0원이 되자마자 파산
+*	4. 완료조건
+*		- 말이 61번째 위치에만 있어야 한다
+*/
+
+
 #include <stdio.h>
 #include <stdlib.h>
-#include<time.h>
-
-/*
-보드게임 만들기 21.05.13
-전체 칸의 갯수 61개
-플레이어가 가지고 있어야 할 것
--위치정보 : 0 부터 시작
--돈 : 5000000 부터 시작
--아이템 인벤토리 (나중)
--파산의 기준
-	0원이 되자마자 파산
-*/
+#include <time.h>
 
 void main(void)
 {
-	//칸의 최대 갯수 상수형 변수 (공간)
+	//칸의 최대갯수, 상수형 변수(공간)
 	const int MAX_LENGTH = 61;
 
 	//플레이어의 위치
@@ -26,21 +29,21 @@ void main(void)
 
 	int money = 5000000;
 
-	srand( time(0) );
+	srand(time(0));
 	rand();
 
 	printf("현재위치 : %d, 가진 돈 : %d\n\n", position, money);
-	
+
 	while (1)
 	{
-		getchar();  //주사위를 던지기 위해서는 엔터키를 눌러야 한다.
+		getchar();		// 주사위를 던지기 위해서는 엔터키를 눌러야 한다.
 		//주사위 던지기 (1부터 6까지)
 		dice = rand() % 6 + 1;
-		position += dice;
+		position = position + dice;
 		printf("주사위를 던져서 %d가 나왔습니다.\n", dice);
 		printf("현재위치 : %d, 가진 돈 : %d\n\n", position, money);
-		
-		//7번에 있을때는 3번으로 이동
+
+		// 7번에 있을때는 3번으로 이동
 		if (position == 7)
 		{
 			printf("3번 위치로 이동합니다.\n");
@@ -54,21 +57,26 @@ void main(void)
 			money -= 2000000;
 			printf("현재위치 : %d, 가진 돈 : %d\n\n", position, money);
 		}
-
-		//파산처리
-		if (money <= 0)
-		{
-			printf("파산하였습니다ㅠㅜㅠㅜ\n");
-			break;
-		}
-
-		//61을 넘으면 목적지에 도착함
 		if (position > 61)
 		{
-			printf("축하합니다. 목적기에 도달하였습니다.\n");
+			printf("61을 넘었끼 때문에 무효처리 합니다.\n");
+			//무효처리를 하기 위한 코드
+			position = position - dice;
+			printf("현재위치 : %d, 가진 돈 : %d\n\n", position, money);
+		}
+		// 파산처리
+		if (money <= 0)
+		{
+			printf("파산 하였습니다ㅠㅠㅠ\n");
 			break;
 		}
-		
-	}// while 문 끝
+
+		// 61 위치에만 있어야 게임을 끝낸다.
+		if (position == 61)
+		{
+			printf("축하합니다. 목적지에 도달하였습니다.\n");
+			break;
+		}
+	} // while문 끝
 	printf("게임이 끝났습니다.");
 }
