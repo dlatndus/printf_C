bf63b4412e66af805d67e43c47c5a7c5a33cd0ac
dlatndus <81746616+dlatndus@users.noreply.github.com>
2021-08-27 15:04:04 +0900

    배열의 기초

---
+++ 문법설명/10-01 배열의 기초.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main(void)
+{
+	int arr[4] = { 10, 20, 30, 40 }; //{}로 초기화
+	//배열의 시작주소값을 의미
+	printf("%d\n", arr);
+
+	printf("%d\n", arr[1]);
+	printf("%d\n", arr[1]);
+	printf("%d\n", arr[2]);
+	printf("%d\n", arr[3]);
+}
