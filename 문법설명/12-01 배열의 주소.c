93ec03ac2f59962b49af64fdb7083f5ba8818d2a
임수연 <81746616+dlatndus@users.noreply.github.com>
2021-11-04 11:06:33 +0900

    arr[1]이랑 arr+1이랑 같다

--- 문법설명/12-01 배열의 주소.c
+++ 문법설명/12-01 배열의 주소.c
@@ -5,16 +5,18 @@ void main(void)
 	int arr[4];
 	//0번째 공간의 주소
 	printf("&arr[0] : %p\n", &arr[0]);
-	printf("arr	: %p\n", arr);//걍 애초에 이게 주소임 걍 같은거
+	printf("arr	: %p\n\n", arr);//걍 애초에 이게 주소임 걍 같은거
 	
 	//1번째 공간의 주소
-	printf("&arr[1] : %p\n\n", &arr[1]);
+	printf("&arr[1] : %p\n", &arr[1]);
+	printf("arr+1 : %p\n\n", arr+1);
 	
 	char str[4];
 	//0번째 공간의 주소
 	printf("&str[0] : %p\n", &str[0]);
-	printf("str	: %p\n", str);
+	printf("str	: %p\n\n", str);
 
 	//1번째 공간의 주소
 	printf("&str[1] : %p\n", &str[1]);
+	printf("str+1 : %p\n", str+1);
 }
