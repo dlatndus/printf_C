7c9c712e7c4c5b9ebc5d1bf3ee7441a0105d2c72
임수연 <81746616+dlatndus@users.noreply.github.com>
2021-11-25 15:10:19 +0900

    동적할당 02

--- 문법설명/14-02 동적할당.c
+++ 문법설명/14-02 동적할당.c
@@ -2,12 +2,17 @@
 
 void main(void)
 {
-	int count; //지역변수 스텍영역에 할당
+	//지역변수에 할당된 배열(스택 영역에 할당)
+	int arr[4] = { 10, 20, 30, 40 };
+
+	int count=4; //지역변수 스텍영역에 할당
 	printf("원하는 int 공간의 갯수를 입력하시오 : ");
-	scanf("%d", &count);
+	//scanf("%d", &count);
 
 	//키보드 입력과정은 프로그램 실행속에 진행됨
 	//malloc 으로 할당된 메모리 크기는 키보드로 입력한 순간에 결정
+	
+	//ptr은 지역변수 스택에 할당
 	int* ptr = malloc(sizeof(int) * count); //4바이트 * 공간 갯수
-
+	ptr[0] = 10; ptr[1] = 20; ptr[2] = 30; ptr[3] = 40;
 }
